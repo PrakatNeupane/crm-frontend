b2ea763fbdf3f4feef2f8fc7356102eaa61b7f2e
PrakatNeupane <neupaneprakat@gmail.com>
2023-09-17 13:52:43 +1000

    layout added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import { Button } from "react-bootstrap";
 import Entry from "./page/entry/Entry.page";
+import DefaultLayout from "./layout/DefaultLayout";
 
 function App() {
   return (
     <div className="App">
-      <Entry />
+      <DefaultLayout>Send stuff </DefaultLayout>
     </div>
   );
 }
---
+++ src/layout/DefaultLayout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Header } from "./partials/Header.comp";
+import { Footer } from "./partials/Footer.comp";
+
+const DefaultLayout = ({ children }) => {
+  return (
+    <div>
+      <div className="header">
+        <Header />
+      </div>
+
+      <div className="main">{children}</div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default DefaultLayout;
